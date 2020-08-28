9f3f7493d0537aaa27406f88b1e8daee25804e39
Chester <chesterdeveloper2000@gmail.com>
2020-08-28 14:39:01 +0800

    used node to dl images from the internet

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
 let fs = require("fs")
+let https = require("https")
 
 
 fs.writeFile(`${__dirname}/index.html`, `<h1>HTML is cool</h1>`, (error) => {
     error ? console.log(error) : console.log("yey! congrats")
 } )
+
+
+const milkyWayImage = `https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg`
+
+https.get(milkyWayImage, (res) => {
+    res.pipe(fs.createWriteStream(`${__dirname}/milkway.jpg`))
+})
